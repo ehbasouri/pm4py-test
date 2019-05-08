0466c412a7bca10d371378ac6ead8d594f1fc934
ehbasouri <ehbasouri@gmail.com>
2019-05-08 20:36:47 +0000

    for push

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 from pm4py.algo.discovery.alpha import factory as alpha_miner
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vis_factory
+from pm4py.algo.discovery.inductive import factory as inductive_miner
 
 log = xes_importer.import_log('./example-logs/exercise3.xes')
-net, initial_marking, final_marking = alpha_miner.apply(log)
+net, initial_marking, final_marking = inductive_miner.apply(log)
 gviz = vis_factory.apply(net, initial_marking, final_marking)
 vis_factory.view(gviz)
 
