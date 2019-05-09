c7e08f686f76a5e4571460033b39f391caef36b3
ehbasouri <ehbasouri.com>
2019-05-09 10:38:57 +0000

    test

--- pm4py-test/main.py
+++ pm4py-test/main.py
@@ -4,7 +4,7 @@ from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vis_factory
 from pm4py.algo.discovery.inductive import factory as inductive_miner
 
-log = xes_importer.import_log('./example-logs/exercise3.xes')
+log = xes_importer.import_log('./example-logs/exercise1.xes')
 net, initial_marking, final_marking = alpha_miner.apply(log)
 gviz = vis_factory.apply(net, initial_marking, final_marking)
 vis_factory.view(gviz)
