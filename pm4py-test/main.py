9dadec9fea520183bb803e91b6e93542ba7bc620
ehbasouri <ehbasouri.com>
2019-05-09 10:48:21 +0000

    test heuristics

--- pm4py-test/main.py
+++ pm4py-test/main.py
@@ -3,9 +3,10 @@ from pm4py.algo.discovery.alpha import factory as alpha_miner
 from pm4py.objects.log.importer.xes import factory as xes_importer
 from pm4py.visualization.petrinet import factory as vis_factory
 from pm4py.algo.discovery.inductive import factory as inductive_miner
+from pm4py.algo.discovery.heuristics import factory as heuristics_miner
 
 log = xes_importer.import_log('./example-logs/exercise1.xes')
-net, initial_marking, final_marking = alpha_miner.apply(log)
+net, initial_marking, final_marking = heuristics_miner.apply(log)
 gviz = vis_factory.apply(net, initial_marking, final_marking)
 vis_factory.view(gviz)
 
